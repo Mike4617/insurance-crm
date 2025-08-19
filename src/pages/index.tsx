d0c84370e82b3e204de97ad001a656ecf66d57c6
Mike4617 <mike@mynextlevelagent.com>
2025-08-19 00:28:57 -0500

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Insurance CRM</h1>
+      <p>Home page is working 🎉</p>
+      <a href="/login">Go to Login</a>
+    </div>
+  );
+}
